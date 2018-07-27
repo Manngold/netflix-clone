30c7e35034c7bfa334767da3c09f0fe74c2f5f1c
Manngold <36980767+Manngold@users.noreply.github.com>
2018-07-28 04:07:32 +0900

    cloning netflix
    final

--- src/server.js
+++ src/server.js
@@ -3,13 +3,16 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const fs = require('fs');
 
-
 const app = express();
 app.use(express.static(path.join(__dirname, '../public/image')));
 app.use(express.static(path.join(__dirname, '../view')));
 app.use(express.static(path.join(__dirname, '../public/video')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/player', (req, res) => {
+    res.sendFile(path.join(__dirname, '../view/player.html'));
 });
 
   
